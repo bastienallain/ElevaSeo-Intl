06306aab073cfc835588038b75c1809e74329446
Vivacoda <75243836+vivacoda@users.noreply.github.com>
2023-12-04 09:19:12 +0100

    Add I18n to NextUI

---
+++ config/fonts.ts
@@ -0,0 +1,14 @@
+import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: 'swap', adjustFontFallback: false
+
+})
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: 'swap', adjustFontFallback: false
+})
--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-// @ts-check
-
 const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+    // Vos configurations Next.js, par exemple:
+    poweredByHeader: false,
+    reactStrictMode: true,
+    // Autres configurations...
+};
 
 module.exports = withNextIntl(config);
